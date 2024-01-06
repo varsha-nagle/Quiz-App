1efd5c00afe98caad5adaec650fa5288feeeac15
varsha-nagle <naglevarsha14@gmail.com>
2024-01-06 17:08:30 +0530

    code for after logging user will be redirect to home page

--- QuizMaster/QuizManager/models.py
+++ QuizMaster/QuizManager/models.py
@@ -3,5 +3,5 @@ from django.forms import ModelForm
 
 # Create your models here.
 class user(ModelForm):
-    field = ['username', 'email']
+    fields = ['username', 'email']
 
--- QuizMaster/QuizManager/urls.py
+++ QuizMaster/QuizManager/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, login_view, register_view
+from .views import index, login_view, register_view, home_view
 
 urlpatterns = [
     path('', index, name='index'),
     path('login/', login_view, name="login"),
-    path('register/', register_view, name="register")
+    path('register/', register_view, name="register"),
+    path('home/', home_view, name="home")
     # other urlpatterns
 ]
