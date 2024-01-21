fa27cc850fe5322e374633129f74902d5cff6b72
varsha-nagle <naglevarsha14@gmail.com>
2024-01-21 23:06:16 +0530

    code for adding models for question, views and redirection of when click on lets start button

--- QuizMaster/QuizManager/models.py
+++ QuizMaster/QuizManager/models.py
@@ -1,7 +1,19 @@
 from django.forms import ModelForm
+from django.db import models
 
 
 # Create your models here.
 class user(ModelForm):
     fields = ['username', 'email']
 
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    correct_option = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.question_text
